e405f3671979a12c47f63d562d8689ea866cd384
wilson.ramirez <wilson.ramirez@truextend.com>
2022-09-02 15:40:01 -0400

    [EX-1] Setup base models

---
+++ exercise-api/Data/Models/Base/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Models.Base
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ exercise-api/Data/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using Data.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Models
+{
+    public class Student : Entity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
