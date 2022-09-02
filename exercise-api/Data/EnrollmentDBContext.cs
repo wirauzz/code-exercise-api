83dae4ca1228913568fca8f025f0d9bfc49177ba
wilson.ramirez <wilson.ramirez@truextend.com>
2022-09-02 16:00:52 -0400

    [EX-1] Created enrollments context

---
+++ exercise-api/Data/EnrollmentDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Data
+{
+    public class EnrollmentDBContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public static EnrollmentDBContext _instance;
+        public DbSet<Student> Student { get; set; }
+        public EnrollmentDBContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("MyConnection"));
+        }
+    }
+}
