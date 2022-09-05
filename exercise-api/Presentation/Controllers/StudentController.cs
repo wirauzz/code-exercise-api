830a973b7dc0328029445dc3ac3b92bad16ee2b4
wilson.ramirez <wilson.ramirez@truextend.com>
2022-09-05 11:38:05 -0400

    [EX-1] Added documentation to endpoints

--- exercise-api/Presentation/Controllers/StudentController.cs
+++ exercise-api/Presentation/Controllers/StudentController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Presentation.Controllers
@@ -21,30 +22,83 @@ namespace Presentation.Controllers
             _studentManager = studentsManager;
         }
 
+        /// <summary>
+        /// Returns all Students
+        /// </summary>
+        /// <remarks>Get all Students</remarks>
+        /// <response code="200">Success or some expected Error</response>
+        [ProducesResponseType(typeof(MiddlewareResponse<IEnumerable<StudentDTO>>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         [HttpGet]
         public async Task<ActionResult> GeAllStudens()
         {
             return Ok(new MiddlewareResponse<IEnumerable<StudentDTO>>(await _studentManager.AllStudents()));
         }
 
+        /// <summary>
+        /// Returns a specific Student
+        /// </summary>
+        /// <remarks>Get Student by Id, Student id must be a unique identifier</remarks>
+        /// <response code="200">Success or some expected Error</response>
+        [ProducesResponseType(typeof(MiddlewareResponse<StudentDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         [HttpGet("{studentId}")]
         public async Task<ActionResult> GetStudent([FromRoute, Required] Guid studentId)
         {
             return Ok(new MiddlewareResponse<StudentDTO>(await _studentManager.StudentById(studentId)));
         }
 
+        /// <summary>
+        /// Creates a new Student and returns it as it is in DB
+        /// </summary>
+        /// <param name="studentDTO">
+        /// The new Student with its information
+        /// ## Body 
+        /// 
+        ///     {
+        ///        "firstName" : "Wilson",
+        ///        "lastName" : "Ramirez",
+        ///     }
+        /// </param>
+        /// <response code="200">Success or some expected Error</response>
+        [ProducesResponseType(typeof(MiddlewareResponse<StudentDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         [HttpPost]
         public async Task<ActionResult> PostStudent([FromBody, Required] StudentDTO studentDTO)
         {
             return Ok(new MiddlewareResponse<StudentDTO>(await _studentManager.Create(studentDTO)));
         }
 
+        /// <summary>
+        /// Deletes a specific Student
+        /// </summary>
+        /// <param name="studentId">Id of the Student to delete.</param>
+        /// <remarks>Delete Student by Id. Student id is a unique identifier</remarks>
+        /// <response code="200">True on success, or some expected Error</response>
+        [ProducesResponseType(typeof(MiddlewareResponse<bool>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         [HttpDelete("{studentId}")]
         public async Task<ActionResult> DeleteStudent([FromRoute, Required] Guid studentId)
         {
             return Ok(new MiddlewareResponse<bool>(await _studentManager.DeleteStudent(studentId)));
         }
 
+        /// <summary>
+        /// Updates an Student and returns it as it is in DB
+        /// </summary>
+        /// <param name="studentId">unique identifier of Student id</param>
+        /// <param name="student">
+        /// Student data to update
+        /// ## Body 
+        /// 
+        ///     {
+        ///        "firstName" : "Wilson",
+        ///        "lastName" : "Ramirez",
+        ///     }
+        /// </param>
+        /// <response code="200">Success or some expected Error</response>
+        [ProducesResponseType(typeof(MiddlewareResponse<StudentDTO>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
         [HttpPut]
         [Route("{studentId}")]
         public async Task<ActionResult> PutStudent([FromRoute, Required] Guid studentId, [FromBody, Required] StudentDTO student)
