c694b4a4c859d3143ba9b3e7e31e44c3da83fc83
wilson.ramirez <wilson.ramirez@truextend.com>
2022-09-05 10:34:20 -0400

    [EX-1] Added exceptions to logic layer

---
+++ exercise-api/Logic/Exceptions/LogicException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Exceptions
+{
+    public class LogicException : Exception
+    {
+        public LogicException(string message) : base(message) { }
+        public LogicException(string message, Exception innerException) : base(string.Format(message), innerException) { }
+    }
+}
--- exercise-api/Logic/Managers/StudentsManager.cs
+++ exercise-api/Logic/Managers/StudentsManager.cs
@@ -1,8 +1,10 @@
 ﻿using Data;
 using Data.Models;
+using Logic.Exceptions;
 using Logic.Managers.Interfaces;
 using Logic.Models;
 using Logic.Models.Mapper;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -30,35 +32,106 @@ namespace Logic.Managers
 
         public async Task<StudentDTO> Create(StudentDTO student)
         {
-            Student newStudent = _mapper.GetDAO(student);
-            Student createResult = await _uow.StudentRepository.CreateAsync(newStudent);
-            StudentDTO createStudent = _mapper.GetDTO(createResult);
-            return createStudent;
+            try
+            {
+                if (string.IsNullOrEmpty(student.FirstName))
+                {
+                    throw new LogicException("You must to add a FirstName to create the student.");
+                }
+                if (string.IsNullOrEmpty(student.LastName))
+                {
+                    throw new LogicException("You must to add a LastName to create this student.");
+                }
+                Student newStudent = _mapper.GetDAO(student);
+                Student createResult = await _uow.StudentRepository.CreateAsync(newStudent);
+                StudentDTO createStudent = _mapper.GetDTO(createResult);
+                return createStudent;
+            }
+            catch (LogicException ex)
+            {
+                string message = $"Error in StudentManager -> Create(){Environment.NewLine} Message: {ex.Message}{Environment.NewLine}";
+                Log.Error(ex, $"{message}{Environment.NewLine} Stack trace: {Environment.NewLine}");
+                throw new LogicException(ex.Message, ex.InnerException);
+            }
         }
 
 
         public async Task<bool> DeleteStudent(Guid studentId)
         {
-            Student studentToDelete = await _uow.StudentRepository.GetByIdAsync(studentId);
-            await _uow.StudentRepository.DeleteAsync(studentToDelete);
-            return await _uow.StudentRepository.GetByIdAsync(studentId) == null;
+            try
+            {
+                Student studentToDelete = await _uow.StudentRepository.GetByIdAsync(studentId);
+                if (studentToDelete == null)
+                {
+                    throw new LogicException($"Can not find any student with id: {studentId}.");
+                }
+                else
+                {
+                    await _uow.StudentRepository.DeleteAsync(studentToDelete);
+                    return await _uow.StudentRepository.GetByIdAsync(studentId) == null;
+                }
+            }
+            catch (LogicException ex)
+            {
+                string message = $"Error in StudentManager -> DeleteStudent(){Environment.NewLine} Message: {ex.Message}{Environment.NewLine}";
+                Log.Error(ex, $"{message}{Environment.NewLine} Stack trace: {Environment.NewLine}");
+                throw new LogicException(ex.Message, ex.InnerException);
+            }
+
         }
 
         public async Task<StudentDTO> StudentById(Guid studentId)
         {
-            Student getByIdResult = await _uow.StudentRepository.GetByIdAsync(studentId);
-            StudentDTO student = _mapper.GetDTO(getByIdResult);
-            return student;
+            try
+            {
+                Student getByIdResult = await _uow.StudentRepository.GetByIdAsync(studentId);
+                if(getByIdResult == null)
+                {
+                    throw new LogicException($"Can not find any Student by id: {studentId}.");
+                }
+                StudentDTO student = _mapper.GetDTO(getByIdResult);
+                return student;
+            }
+            catch (LogicException ex)
+            {
+                string message = $"Error in StudentManager -> StudentById(){Environment.NewLine} Message: {ex.Message}{Environment.NewLine}";
+                Log.Error(ex, $"{message}{Environment.NewLine} Stack trace: {Environment.NewLine}");
+                throw new LogicException(ex.Message, ex.InnerException);
+            }
         }
 
         public async Task<StudentDTO> UpdateById(Guid studentId, StudentDTO student)
         {
-            Student studentToEdit = await _uow.StudentRepository.GetByIdAsync(studentId);
-            studentToEdit.FirstName = student.FirstName;
-            studentToEdit.LastName = student.LastName;
-            Student updateResult = await _uow.StudentRepository.UpdateAsync(studentToEdit);
-            StudentDTO editedStudent = _mapper.GetDTO(updateResult);
-            return editedStudent;
+            try
+            {
+                Student studentToEdit = await _uow.StudentRepository.GetByIdAsync(studentId);
+                if (studentToEdit == null)
+                {
+                    throw new LogicException($"The Student request id {studentId} provided to update doesn't exist.");
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(student.FirstName))
+                    {
+                        throw new LogicException("You must to add a FirstName to create the student.");
+                    }
+                    if (string.IsNullOrEmpty(student.LastName))
+                    {
+                        throw new LogicException("You must to add a LastName to create this student.");
+                    }
+                    studentToEdit.FirstName = student.FirstName;
+                    studentToEdit.LastName = student.LastName;
+                    Student updateResult = await _uow.StudentRepository.UpdateAsync(studentToEdit);
+                    StudentDTO editedStudent = _mapper.GetDTO(updateResult);
+                    return editedStudent;
+                }
+            }
+            catch(LogicException ex)
+            {
+                string message = $"Error in StudentManager -> UpdateById(){Environment.NewLine} Message: {ex.Message}{Environment.NewLine}";
+                Log.Error(ex, $"{message}{Environment.NewLine} Stack trace: {Environment.NewLine}");
+                throw new LogicException(ex.Message, ex.InnerException);
+            }
         }
     }
 }
