c296fed87f7201053f06ac74a3f2dd8663ff1c1e
wilson.ramirez <wilson.ramirez@truextend.com>
2022-09-04 21:32:04 -0400

    [EX-1] Generated DTO and DAO models

---
+++ exercise-api/Logic/Models/StudentDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Models
+{
+    public class StudentDTO
+    {
+        public Guid StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
