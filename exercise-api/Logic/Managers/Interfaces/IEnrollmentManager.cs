2032c05d2a2dd2f47c460723f0e9a89165156fcf
wilson.ramirez <wilson.ramirez@truextend.com>
2022-09-07 13:37:13 -0400

    [EX-6] Added logic layer to enrollments

---
+++ exercise-api/Logic/Managers/Interfaces/IEnrollmentManager.cs
@@ -0,0 +1,20 @@
+﻿using Data.Models;
+using Logic.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Managers.Interfaces
+{
+    public interface IEnrollmentManager
+    {
+        Task<IEnumerable<EnrollmentDTO>> AllEnrollments();
+        Task<IEnumerable<Class>> SearchEnrollmentsByUser(Guid studentId);
+        Task<IEnumerable<Student>> SearchEnrollmentsByCourse(Guid courseId);
+        Task<EnrollmentDTO> Create(EnrollmentDTO newEnrollment);
+        Task<EnrollmentDTO> UpdateById(Guid enrollmentId, EnrollmentDTO enrollmentStateDTO);
+        Task<EnrollmentDTO> EnrollmentById(Guid enrollmentId);
+        Task<bool> DeleteEnrollment(Guid enrollmentId);
+    }
+}
---
+++ exercise-api/Logic/Models/Mapper/EnrollmentMapper.cs
@@ -0,0 +1,41 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic.Models.Mapper
+{
+    public class EnrollmentMapper : IMapper<EnrollmentDTO, Enrollment>
+    {
+        public Enrollment GetDAO(EnrollmentDTO enrollment)
+        {
+            return new Enrollment()
+            {
+                Id = enrollment.Id,
+                CourseId = enrollment.CourseId,
+                StudentId = enrollment.StudentId
+            };
+        }
+
+        public EnrollmentDTO GetDTO(Enrollment enrollment)
+        {
+            EnrollmentDTO enrollmentDTO = new EnrollmentDTO()
+            {
+                Id = enrollment.Id,
+                CourseId = enrollment.CourseId,
+                StudentId = enrollment.StudentId
+            };
+            return enrollmentDTO;
+        }
+
+        public IEnumerable<EnrollmentDTO> GetDTO(IEnumerable<Enrollment> enrollments)
+        {
+            List<EnrollmentDTO> enrollmentDTOs = new List<EnrollmentDTO>();
+            foreach (Enrollment enrollment in enrollments)
+            {
+                enrollmentDTOs.Add(GetDTO(enrollment));
+            }
+            return enrollmentDTOs;
+        }
+    }
+}
